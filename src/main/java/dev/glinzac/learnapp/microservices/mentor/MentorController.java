9b3b008ec897d1665b57dc57cc683bfb7400fede
glinzachariah <glinzac@gmail.com>
2019-09-20 17:23:00 +0530

    checkpoint course

--- src/main/java/dev/glinzac/learnapp/microservices/mentor/MentorController.java
+++ src/main/java/dev/glinzac/learnapp/microservices/mentor/MentorController.java
@@ -1,5 +1,21 @@
 package dev.glinzac.learnapp.microservices.mentor;
 
-public class MentorController {
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import dev.glinzac.learnapp.models.CourseModel;
+
+@RestController
+@RequestMapping("/mentor")
+public class MentorController {
+	
+//	addCourse
+	@RequestMapping(name = "/addCourse",method = RequestMethod.POST)
+	public void addCourse(@RequestBody CourseModel course) {
+		
+	}
+//	getCoursesList
+//	deleteCourse
 }
---
+++ src/main/java/dev/glinzac/learnapp/models/CourseModel.java
@@ -0,0 +1,16 @@
+package dev.glinzac.learnapp.models;
+
+import dev.glinzac.learnapp.entities.Technology;
+
+public class CourseModel {
+//courseid: string;
+//coursename: string;
+//trainername: string;
+//technology: string;
+//charges: number;
+//commission:number;
+	private String courseid;
+	private String courseName;
+	private String trainerName;
+	private Technology technology;
+}
